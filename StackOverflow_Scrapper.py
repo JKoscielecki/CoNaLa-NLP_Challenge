beab3c56c0c62a6431b78c128a7f8f3f38a7809b
Jakub Koscielecki <koscielecki.jakub@gmail.com>
2023-01-27 14:44:21 +0100

    deleting redundant file

--- StackOverflow_Scrapper.py
+++
@@ -1,5 +0,0 @@
-# import os
-from selenium import webdriver
-# os.environ['PATH'] += r"C:\SeleniumDrivers"
-driver = webdriver.Chrome()
-driver.get("https://stackoverflow.com/questions/tagged/python?tab=Votes")
